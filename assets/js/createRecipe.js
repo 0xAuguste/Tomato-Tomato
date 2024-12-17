50da5a022157aa702dddf7ce1034fad4c8462b29
0xAuguste <gusinator9@gmail.com>
2024-12-16 17:16:47 -0800

    Brake js into separate files and create assets & backend file structure

---
+++ assets/js/createRecipe.js
@@ -0,0 +1,181 @@
+printOptions(document.getElementById("add-ingred-unit"), 'unit', 'name');
+printOptions(document.getElementById("add-ingred-name"), 'ingredient', 'ingred_name');
+let recipeData = new RecipeData();
+
+// FUNCTION DEFINITIONS
+
+// Toggles display of #add-ingredient-panel
+function openAddIngredient() {
+    var panel = document.getElementById("add-ingredient-panel");
+    panel.style.display = "block";
+}
+// Toggles display of #add-ingredient-panel
+function closeAddIngredient() {
+    var panel = document.getElementById("add-ingredient-panel");
+    panel.style.display = "none";
+}
+// Toggles display of #create-ingredient-panel
+function openCreateIngredient() {
+    var panel = document.getElementById("create-ingredient-panel");
+    panel.style.display = "block";
+}
+// Toggles display of #create-ingredient-panel
+function closeCreateIngredient() {
+    var panel = document.getElementById("create-ingredient-panel");
+    panel.style.display = "none";
+}
+// Main function to handle text entry into recipe body divs
+// Handles "Enter", "Shift + Enter", & "Delete" functionality
+function textHandler(e, elem) {
+    if (e.key === "Enter") {
+        e.preventDefault();
+
+        if (e.shiftKey && elem.parentElement.id === "recipe-process") {
+            openAddIngredient();
+        }
+        else {
+            var newParagraph = document.createElement("p");
+            newParagraph.classList.add("recipe-paragraph");
+            newParagraph.setAttribute('contenteditable', 'true');
+            newParagraph.setAttribute('onkeydown', 'textHandler(event, this)');
+            elem.after(newParagraph);
+            newParagraph.focus();
+        }
+    }
+    else if (e.key === "Backspace" || e.key === "Delete") {
+        if (elem.textContent == "" && elem.previousElementSibling !== null) {
+            elem.previousElementSibling.focus();
+            elem.remove();
+        }
+        else if (elem.children.length && elem.children[0].innerText === elem.innerText) {
+            recipeData.removeIngredientByID(elem.children[0].id); // remove ingredient from data
+            elem.children[0].remove(); // remove ingredient text span
+        }
+    }
+}
+// Handles user clicks in recipe body text divs to allow user to select appropriate <p> tags
+function textBoxClick(e, elem) {
+    if (e.target === elem) {
+        paragraphs = [];
+        for (child of elem.children) {
+            if (child.tagName == "P") {
+                paragraphs.push(child);
+            }
+        }
+
+        paragraphs.pop().focus();
+    }
+}
+function optionFilter(elem, tableName, colName) {
+    let queryString = elem.value.toUpperCase();
+    let list = elem.parentElement.getElementsByTagName('ul')[0];
+    list.innerHTML = ""; // clear old options
+
+    let options = getOptionList(tableName, colName);
+    console.log(options);
+    for (row of options) {
+        let optionText = row[colName].toUpperCase();
+        if (optionText.indexOf(queryString) > -1) {
+            let listItem = document.createElement("li");
+            listItem.classList.add("dropdown-option");
+            list.add(listItem);
+        }
+    }
+}
+function getOptionList(tableName, colName) {
+    let ajax_request = new XMLHttpRequest();
+    ajax_request.open('POST', '/backend/DB/get_options.php');
+    let form_data = new FormData();
+    form_data.append("table", tableName);
+    form_data.append("column", colName);
+
+    ajax_request.send(form_data);
+    ajax_request.onreadystatechange = function() {
+        if (ajax_request.readyState == 4 && ajax_request.status == 200) {
+            json_response = JSON.parse(ajax_request.responseText);
+        }
+    }
+
+    return json_response;
+}
+// Helper function to convert an all lowercase work to a capitalized first letter
+function capitalize(word) {
+    return word.charAt(0).toUpperCase()+ word.slice(1);
+}
+// Pushes new ingredient info entered by the user to the database
+function saveNewIngredient() {
+    let form_element = document.getElementsByClassName('new-ingred-form');
+    let form_data = new FormData();
+
+    for (let i = 0; i < form_element.length; i++) {
+        form_data.append(form_element[i].name, form_element[i].value);
+    }
+
+    let ajax_request = new XMLHttpRequest();
+    ajax_request.open('POST', '/backend/DB/new_ingredient.php');
+    ajax_request.send(form_data);
+    ajax_request.onreadystatechange = function() {
+        if (ajax_request.readyState == 4 && ajax_request.status == 200) {
+            document.getElementById('new-ingredient-form').reset();
+            closeCreateIngredient();
+            alert(ajax_request.responseText);
+        }
+    }
+}
+// Adds ingredient to the recipe
+function saveAddIngredient() {
+    let newIngredient = document.createElement("p");
+    newIngredient.classList.add("recipe-paragraph");
+    newIngredient.setAttribute('contenteditable', 'true');
+    newIngredient.setAttribute('onkeydown', 'textHandler(event, this)');
+    let ingredientText = document.createElement("span");
+    ingredientText.setAttribute('contenteditable', 'false');
+    ingredientText.classList.add("ingredient-text");
+    ingredientText.setAttribute('onclick', 'editIngredient(this)');
+    let id = Date.now();
+    ingredientText.id = id;
+
+    let name = document.getElementById("add-ingred-name").value;
+    let quantity = document.getElementById("add-ingred-quantity").value;
+    let unit = document.getElementById("add-ingred-unit").value;
+
+    let ingredient = new Ingredient(id, name, quantity, unit);
+    recipeData.addIngredient(ingredient);
+
+    ingredientText.innerText = document.getElementById("add-ingred-display").value;
+    newIngredient.append(ingredientText);
+    let previousPara = document.getElementById("add-ingredient-panel").previousElementSibling
+    previousPara.after(newIngredient);
+    if (previousPara.innerText === "") {
+        previousPara.remove();
+    }
+
+    closeAddIngredient();
+    newIngredient.focus();
+    console.log(recipeData);
+}
+// Opens ingredient editor for clicked ingredient
+function editIngredient(ingred) {
+    
+}
+// Pulls a column from the database and adds each row as an option to a given <select> element
+function printOptions(parent, tableName, colName) {
+    let ajax_request = new XMLHttpRequest();
+    ajax_request.open('POST', '/backend/DB/get_options.php');
+    let form_data = new FormData();
+    form_data.append("table", tableName);
+    form_data.append("column", colName);
+
+    ajax_request.send(form_data);
+    ajax_request.onreadystatechange = function() {
+        if (ajax_request.readyState == 4 && ajax_request.status == 200) {
+            console.log(ajax_request.responseText);
+            for (row of JSON.parse(ajax_request.responseText)) {
+                let option = document.createElement("option");
+                option.classList.add("select-option");
+                option.innerText = row[colName];
+                parent.add(option);
+            }
+        }
+    }
+}
--- backend/DB/get_options.php
+++ backend/DB/get_options.php
@@ -2,19 +2,15 @@
 
 if(isset($_POST['table'])) { 
     try {
-        define('__ROOT__', dirname(dirname(__FILE__)));
-        require_once(__ROOT__.'/databaseKeys.php');
+        require_once('databaseKeys.php');
 
         $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
 
         $sth = $dbh->prepare("SELECT {$_POST['column']} FROM {$_POST['table']}");
-        // $sth->bindValue(':col', $_POST['column']);
-        // $sth->bindValue(':tab', $_POST['table']);
         $sth->execute();
         $options = $sth->fetchAll();
 
         echo json_encode($options);
-
     }
     catch(PDOException $e) {
         echo $e->getMessage();
